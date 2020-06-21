50763871bc014978b7189365878e92d9c0af20e9
Hopson Gausi <57468358+hopgausi@users.noreply.github.com>
2020-06-22 00:05:58 +0200

    Update mourits_api.py

--- lyrics/mourits_api.py
+++ lyrics/mourits_api.py
@@ -2,5 +2,5 @@ import requests
 url = "https://mourits-lyrics.p.rapidapi.com/"
 headers = {
     'x-rapidapi-host': "mourits-lyrics.p.rapidapi.com",
-    'x-rapidapi-key': "207bae272fmsh057bc6676cc787fp184439jsnc6c458bcf447"
+    'x-rapidapi-key': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     }
